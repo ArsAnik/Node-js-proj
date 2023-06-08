6571af09eaa37739c4ebac260893661f371a2d57
FedorKam <116003118+ArsAnik@users.noreply.github.com>
2023-06-08 17:19:30 +0400

    file structure improvement
    add information to Readme
    add documentation files

---
+++ controllers/authController.js
@@ -0,0 +1,31 @@
+class AuthController{
+    async registration(req, res){
+        try {
+
+        }
+        catch (e)
+        {
+            
+        }
+    }
+    async login(req, res){
+        try {
+
+        }
+        catch (e)
+        {
+
+        }
+    }
+    async getUsers(req, res){
+        try {
+            res.json("Users")
+        }
+        catch (e)
+        {
+
+        }
+    }
+}
+
+module.exports = new AuthController()
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express')
+const mysql = require('mysql2')
+const authRouter = require('./routes/authRouter')
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())
+app.use('/auth', authRouter)
+
 const start = () => {
     try {
-       app.listen(PORT, () => console.log(`Сервер запущен на порте ${PORT}`))
+       app.listen(PORT, ( => console.log(`Сервер запущен на порте ${PORT}`))
     }
     catch (e) {
         console.log(e)
     }
 }
+
+start()
